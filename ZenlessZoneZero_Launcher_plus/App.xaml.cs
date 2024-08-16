edf9675eaf536832d09787b651afd1b9b6d32c47
catboss <1004148094@qq.com>
2024-08-16 23:44:00 +0800

    start

---
+++ ZenlessZoneZero_Launcher_plus/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ZenlessZoneZero_Launcher_plus
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
